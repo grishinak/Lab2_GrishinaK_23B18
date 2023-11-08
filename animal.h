4d6f6e21e2cc4d8798453f20121717bad26aff7d
grishinak <147039836+grishinak@users.noreply.github.com>
2023-11-08 12:52:04 -0700

    Add files via upload

---
+++ animal.h
@@ -0,0 +1,56 @@
+#pragma once
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:
+    Animal()//constructor po umolchaniyu
+    {
+
+    }
+    ~Animal()//destructor po umolchaniyu
+    {
+    }
+
+    void makeSound() {
+        cout << "animal makesound function" << endl;
+    }
+private:
+    int age;
+    string name;
+};
+
+class Pet : public Animal {
+public:
+
+
+    void play() {
+        cout << "pet play function" << endl;
+    }
+private:
+    bool hasOwner;
+};
+
+class Dog : public Pet {
+public:
+
+    void bark() {
+       cout << "dog bark function" << endl;
+    }
+private:
+    string breed;
+};
+
+class Puppy : public Dog {
+public:
+ 
+
+    void wagTail() {
+        cout << "puppy wigtail function" << endl;
+    }
+};
+
