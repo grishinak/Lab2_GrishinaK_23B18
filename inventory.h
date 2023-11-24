acc2f2f08bafefac70ca762804542a548f21e248
grishinak <147039836+grishinak@users.noreply.github.com>
2023-11-24 10:56:47 -0800

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -4,11 +4,9 @@
 #include <iostream>
 #include "weapon.h"
 
-using namespace std;
-
-
 class Inventory {
 public:
+
     void AddWeapon(const Weapon& weapon) {
         weapons.push_back(weapon);
     }
@@ -20,19 +18,3 @@ public:
 private:
     vector<Weapon> weapons;
 };
-void TestInventory() {
-    Inventory inventory;
-    Weapon sword("Sword", 20);
-    Weapon axe("Axe", 30);
-
-    inventory.AddWeapon(sword);
-    inventory.AddWeapon(axe);
-
-    assert(inventory.GetWeapons().size() == 2);
-
-    // 
-    assert(inventory.GetWeapons()[0].GetDamage() == 20);
-    assert(inventory.GetWeapons()[1].GetDamage() == 30);
-
-    cout << "inventory test passed" << endl;
-}
