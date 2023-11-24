a0d867e7e348b44f20e595833763349e1265ce0a
grishinak <147039836+grishinak@users.noreply.github.com>
2023-11-24 10:57:23 -0800

    Update monster.h

--- monster.h
+++ monster.h
@@ -3,16 +3,7 @@
 #include <cassert>
 #include "character.h"
 
-
 class Monster : public Character {
 public:
     Monster(string name, int health, int damage) : Character(name, health, damage) {}
 };
-void TestMonster() {
-    Monster goblin("Goblin", 30, 5);
-    assert(goblin.GetName() == "Goblin");
-    assert(goblin.GetHealth() == 30);
-    assert(goblin.GetDamage() == 5);
-
-    cout << "Monster test passed" << endl;
-}
