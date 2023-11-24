e6895e2d413c0093a8242cf94d133e52821c93ba
grishinak <147039836+grishinak@users.noreply.github.com>
2023-11-24 10:50:00 -0800

    Update animal.h

--- animal.h
+++ animal.h
@@ -1,56 +1,35 @@
 #pragma once
-
-
 #include <iostream>
 #include <string>
+
 
 using namespace std;
 
 class Animal {
 public:
-    Animal()//constructor po umolchaniyu
+ 
+    int GetAge() 
     {
-
+        return age;
     }
-    ~Animal()//destructor po umolchaniyu
+
+    void SetAge(int valueAge);
+
+    string GetName()
     {
+        return name;
+    }
+
+    void SetName(string valueName)
+    {
+        name = valueName;
     }
 
     void makeSound() {
         cout << "animal makesound function" << endl;
     }
+    
 private:
     int age;
     string name;
 };
-
-class Pet : public Animal {
-public:
-
-
-    void play() {
-        cout << "pet play function" << endl;
-    }
-private:
-    bool hasOwner;
-};
-
-class Dog : public Pet {
-public:
-
-    void bark() {
-       cout << "dog bark function" << endl;
-    }
-private:
-    string breed;
-};
-
-class Puppy : public Dog {
-public:
- 
-
-    void wagTail() {
-        cout << "puppy wigtail function" << endl;
-    }
-};
-
