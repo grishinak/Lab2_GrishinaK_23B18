5469f67661c6eb959162734394136fbac47f4e50
grishinak <147039836+grishinak@users.noreply.github.com>
2023-12-01 07:59:23 -0800

    Update boss.h

--- boss.h
+++ boss.h
@@ -11,3 +11,12 @@ public:
 private:
     int bossLevel;
 };
+void TestBoss() {
+    Boss dragon("Dragon Boss", 150, 30, 5);
+    assert(dragon.GetName() == "Dragon Boss");
+    assert(dragon.GetHealth() == 150);
+    assert(dragon.GetDamage() == 30);
+    assert(dragon.GetBossLevel() == 5);
+
+    cout << "Boss test passed." << endl;
+}
