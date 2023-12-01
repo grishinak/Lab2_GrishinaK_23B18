13a49be90447ddcacd5b4f072415e6f7369ade8c
grishinak <147039836+grishinak@users.noreply.github.com>
2023-12-01 08:01:05 -0800

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -1,12 +1,15 @@
 #pragma once
+#pragma once
 #include <cassert>
 #include <vector>
 #include <iostream>
 #include "weapon.h"
 
+using namespace std;
+
+
 class Inventory {
 public:
-
     void AddWeapon(const Weapon& weapon) {
         weapons.push_back(weapon);
     }
@@ -18,3 +21,19 @@ public:
 private:
     vector<Weapon> weapons;
 };
+void TestInventory() {
+    Inventory inventory;
+    Weapon sword("Sword", 20);
+    Weapon axe("Axe", 30);
+
+    inventory.AddWeapon(sword);
+    inventory.AddWeapon(axe);
+
+    assert(inventory.GetWeapons().size() == 2);
+
+    // 
+    assert(inventory.GetWeapons()[0].GetDamage() == 20);
+    assert(inventory.GetWeapons()[1].GetDamage() == 30);
+
+    cout << "inventory test passed" << endl;
+}
