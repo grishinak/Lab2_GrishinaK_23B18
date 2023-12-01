81233a9e6287f81cc602fee080e5bcf489463dbc
grishinak <147039836+grishinak@users.noreply.github.com>
2023-12-01 07:59:01 -0800

    Update animal.h

--- animal.h
+++ animal.h
@@ -1,35 +1,18 @@
 #pragma once
+
 #include <iostream>
 #include <string>
-
 
 using namespace std;
 
 class Animal {
 public:
- 
-    int GetAge() 
-    {
-        return age;
-    }
-
-    void SetAge(int valueAge);
-
-    string GetName()
-    {
-        return name;
-    }
-
-    void SetName(string valueName)
-    {
-        name = valueName;
-    }
 
     void makeSound() {
         cout << "animal makesound function" << endl;
     }
-    
 private:
     int age;
     string name;
 };
+
