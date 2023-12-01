ed32a285563a20754afb95083923272e78f30a99
grishinak <147039836+grishinak@users.noreply.github.com>
2023-12-01 08:03:28 -0800

    Update weapon.h

--- weapon.h
+++ weapon.h
@@ -3,26 +3,24 @@
 #include <cassert>
 #include <iostream>
 
+using namespace std;
 
 class Weapon {
 public:
     Weapon(string name, int damage) : name(name), damage(damage) {}
 
-    string GetName()
-    {
-        return name;
-    }
-
-    void SetName(string valueName);
-
-    int GetDamage() const
-    {
+    int GetDamage() const {
         return damage;
     }
-
-    void SetDamage(int valueDamage);
 
 private:
     string name;
     int damage;
 };
+void TestWeapon() {
+    Weapon sword("Sword", 20);
+
+    assert(sword.GetDamage() == 20);
+
+    cout << "Weapon test passed." << endl;
+}
