89a840b9a8beb17c539c55c01385b36171b287bd
grishinak <147039836+grishinak@users.noreply.github.com>
2023-12-01 08:02:35 -0800

    Update pet.h

--- pet.h
+++ pet.h
@@ -4,14 +4,10 @@
 class Pet : public Animal {
 public:
 
+
     void play() {
         cout << "pet play function" << endl;
     }
-
-    bool GetHasOwner();
-
-    void SetHasOwner(bool valueHasOwner);
-
 private:
     bool hasOwner;
 };
